dc0af960af602aae93463164a4489d8f2c1a51c9
longmaker2 <l.deng@alustudent.com>
2023-04-01 17:06:56 +0200

    Web stack debugging #4

---
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+It allows you to simulate HTTP requests to a web server
+
+exec { 'replace_limit':
+  path    => '/usr/bin:/usr/sbin:/bin',
+  command => 'sed -i "/ULIMIT=/c\ULIMIT=\'-n 4096\'" /etc/default/nginx; service nginx restart',
+}
