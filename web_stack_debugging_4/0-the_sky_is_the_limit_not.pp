b8c38f148556f6fc2056b04eb4b7020908065d14
longmaker2 <l.deng@alustudent.com>
2023-04-01 17:08:56 +0200

    Web stack debugging #4

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
-It allows you to simulate HTTP requests to a web server
+# It allows you to simulate HTTP requests to a web server
 
 exec { 'replace_limit':
   path    => '/usr/bin:/usr/sbin:/bin',
